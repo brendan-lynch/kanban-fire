2c58d9e26d8dc7b71e0b09ec1360db07e91ed1a4
Brendan Lynch <blynch87@gmail.com>
2024-02-25 23:18:30 -0500

    Tasks exist without database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,36 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { Task } from './task/task';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
   imports: [
     CommonModule,
     RouterOutlet,
     MatToolbarModule,
-    MatIconModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    MatIconModule,
+    MatCardModule,
+    TaskComponent
+  ]
 })
 export class AppComponent {
   title = 'kanban-fire';
+  todo: Task[] = [
+    {
+      title: 'Buy milk',
+      description: 'Go to the store and buy milk',
+      id: ''
+    },
+    {
+      title: 'Create a Kanban app',
+      description: 'Using Firebase and Angular create a Kanban app!',
+      id: ''
+    }
+  ];
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from './task';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [MatCardModule, CommonModule],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input() task: Task | null = null;
+  @Output() edit = new EventEmitter<Task>();
+}
